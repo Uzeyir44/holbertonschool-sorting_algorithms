d977d5947e3ea1821c6b25b4a87ab9ed20e2bcf2
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-02-07 18:31:17 +0400

    betty and time coplexity in file 2-0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,35 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: the array to sort
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, index;
-    int min, flag;
+	size_t i, j, index;
+	int min, flag;
 
-    for (i = 0; i < size; i++)
-    {
-        min = array[i];
-        index = i;
-        flag = 0;
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		index = i;
+		flag = 0;
 
-        for (j = i+1; j < size; j++)
-        {
-            if (array[j] < min)
-            {
-                min = array[j];
-                index = j;
-                flag = 1;
-            }
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				index = j;
+				flag = 1;
+			}
+		}
 
-        array[index] = array[i];
-        array[i] = min;
+		array[index] = array[i];
+		array[i] = min;
 
-        if (flag)
-            print_array(array, size);
-    }
+		if (flag)
+			print_array(array, size);
+	}
 }
